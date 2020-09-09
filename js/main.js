74d2bc72991cd269cca8a700802a50a529c93b30
Kulbear <jiyang.py@gmail.com>
2020-09-09 13:24:25 -0600

    Fix inaccurate raycaster with resized canvas

--- js/main.js
+++ js/main.js
@@ -67,8 +67,9 @@ document.addEventListener('mousemove', onDocumentMouseMove, false);
 
 function onDocumentMouseMove(event) {
     event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    mouse.x = ((event.clientX - renderer.domElement.offsetLeft) / renderer.domElement.width) * 2 - 1;
+    mouse.y = -((event.clientY - renderer.domElement.offsetTop) / renderer.domElement.height) * 2 + 1;
 }
 
 
@@ -207,9 +208,6 @@ function renderObject(object) {
 
 function loadAndRenderObject() {
     const objLoader = new OBJLoader2();
-    // TODO: load from local fsys
-    // const path = 'models/286/objs/source.obj';
-    // const model_id = 20390;
     const model_id = document.getElementById("modelIdInput").value;
 
     const path = `models/${model_id}/objs/source.obj`;
