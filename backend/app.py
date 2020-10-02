e323363a5c1d0c22590d59d63a0a1a72aaca7642
Kulbear <jiyang.py@gmail.com>
2020-10-02 16:09:29 -0600

    Add save annotation functionality backend

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from find_candidate_joints import part_joints
 import json
+
 app = Flask(__name__)
 CORS(app)
 
-# MODEL_FILE_PATH = 'C:/Users/Darker White/Documents/GitHub/ModelAnnotator/models'
 MODEL_FILE_PATH = '../models'
+
 
 @app.route('/')
 def index():
     return "Hello, World!"
 
+
 @app.route('/api/v0.1/candidate_joints/<int:model_id>')
 def get_candidate_joints(model_id):
-    fpath = f'{MODEL_FILE_PATH}/{model_id}'
-    
-    fname_label = fpath + '/point_sample/label-10000.txt'
-    fname_verts = fpath + '/point_sample/pts-10000.txt'
+        fpath = f'{MODEL_FILE_PATH}/{model_id}'
 
-    with open(fpath + '/meta.json') as f:
-        data = json.load(f)
+        fname_label = fpath + '/point_sample/label-10000.txt'
+        fname_verts = fpath + '/point_sample/pts-10000.txt'
 
-    array = part_joints(fname_label, fname_verts, save_name='test2')
-    return jsonify({
-        'joints': array.tolist(),
-        'model_cat': data['model_cat']
-        }) 
+        with open(fpath + '/meta.json') as f:
+            data = json.load(f)
+
+        array = part_joints(fname_label, fname_verts, save_name='test2')
+        return jsonify({
+            'joints': array.tolist(),
+            'model_cat': data['model_cat']
+        })
+
+@app.route('/api/v0.1/save_joints/<int:model_id>', methods=['POST'])
+def save_candidate_joints(model_id):
+        data = request.data.decode('utf-8')
+        with open(f'../{model_id}.json', 'w') as f:
+            data = json.loads(data)
+            json.dump(data, f)
+        return jsonify({
+            'status': 200
+        })
 
 
 if __name__ == '__main__':
